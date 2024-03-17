03a279acaffc9b2d3948bb4a5f916577b417f433
Gtalen <great.talen@gmail.com>
2024-03-17 19:16:32 +0000

    learning

--- task4/collatz.py
+++ task4/collatz.py
@@ -16,8 +16,36 @@ while number != 1:
 #applies to even numbers
     if  number % 2 == 0:
        number = number/ 2
-         number =("[:.0f] {}"format.number))
+       #number =("[:.0f] {}"format.number))
 #applies to odd numbers
     else:
         number = ((number * 3) + 1)/2
 print (1)
+
+
+
+# option 2 PENDING COMPLETION
+#The line below prompts the  user for a number input
+def numbers(number):
+    number = int(input("Enter a positive number: "))
+
+
+#ensures that only a positive integer is entered
+while number <= 0:
+    print ("Please enter a positive integer: ")
+    number = int(input("Enter a positive number: "))
+#stores the number in a list
+#numbers = [number]
+
+#defines the condition statement
+while number != 1:
+    print (number)
+    if  number % 2 == 0:    #applies to even numbers
+       number = number// 2
+    else:                   #applies to odd numbers
+        number = ((number * 3) + 1)/2
+        #numbers.append(number) #appends number to list
+        print (numbers)
+print (1)
+
+
---
+++ task5/lists.py
@@ -0,0 +1,5 @@
+a = [23, 45, 'boat', 'noah', [1,2,3], {}]
+print (type (a))
+a.append([4,5,6])
+print (len(a))
+print (a)

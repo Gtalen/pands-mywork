5c8c360d007a7db0a0a936233aa03b11edec8f84
Gtalen <great.talen@gmail.com>
2024-05-16 23:24:14 +0100

    data integrity check

---
+++ lag.py
@@ -0,0 +1,5 @@
+// For Visual Studio Code
+codesign --remove-signature /Applications/Visual\ Studio\ Code.app/Contents/Frameworks/Code\ Helper\ \(Renderer\).app
+
+// For Visual Studio Code - Insiders
+codesign --remove-signature /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Frameworks/Code\ -\ Insiders\ Helper\ \(Renderer\).app
